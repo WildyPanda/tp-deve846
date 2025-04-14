4c35e623c63752e67dbc87e7bb4ea5f57745a473
WildyPanda <65488309+WildyPanda@users.noreply.github.com>
2025-04-14 14:39:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    json_file_path = os.path.join(app.root_path, 'your_file.json')
+    json_file_path = os.path.join(app.root_path, 'logs_large.json')
     return send_file(json_file_path, mimetype='application/json')
